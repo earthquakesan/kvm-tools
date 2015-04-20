aa8e29222b6910e6ffb7a688e0f1c169b2a63b10
MSherif <mohameddotsherif@gmail.com>
2015-04-20 10:08:38 +0200

    added unit='' for currentMemory

--- collect.sh
+++ collect.sh
@@ -3,8 +3,9 @@
 FILES=`ls /etc/libvirt/qemu/*.xml`
 for FILE in $FILES
 do
+	#cat $FILE
 	NAME=`grep -oh "<name>.*</name>" $FILE | sed 's/\(<name>\|<\/name>\)//g'`
-	CURRENTMEMORY=`grep -oh "<currentMemory>.*</currentMemory>" $FILE | sed 's/\(<currentMemory>\|<\/currentMemory>\)//g'`
+	CURRENTMEMORY=`grep -oh "<currentMemory.*>.*</currentMemory>" $FILE | sed 's/\(<currentMemory>\|<currentMemory unit=.KiB.>\|<\/currentMemory>\)//g'`
 	#echo "$NAME"
 	echo "$CURRENTMEMORY"
 done
